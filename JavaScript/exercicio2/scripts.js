40d93ae804c2327aadc5ad2348d895f586cf5c3c
Matheus R. Ouverney <162365159+Matheuzin18@users.noreply.github.com>
2024-04-09 18:49:14 -0300

    Delete JavaScript/exercicio2 directory

--- JavaScript/exercicio2/scripts.js
+++
@@ -1,9 +0,0 @@
-function metro() {
-    var m = document.getElementById("metros").value;
-    var cm = m*100;
-    document.getElementById("resultado").textContent = `${m} metros equivalem a ${cm} centímetros.`;
-
-   console.log(m);
-   console.log(cm);
-   
-}
