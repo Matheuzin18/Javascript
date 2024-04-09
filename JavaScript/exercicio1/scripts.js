ded5cdaed272d8fdb69eeb2122d1500c595678d3
Matheus R. Ouverney <162365159+Matheuzin18@users.noreply.github.com>
2024-04-09 18:48:53 -0300

    Delete JavaScript/exercicio1 directory

--- JavaScript/exercicio1/scripts.js
+++
@@ -1,9 +0,0 @@
-function nascimento() {
-    var idade = document.getElementById("idade").value;
-    var calculo = 2024 - idade;
-    document.getElementById("resultado").innerHTML = calculo;
-
-
-    console.log(idade);
-    console.log(calculo);
-}
